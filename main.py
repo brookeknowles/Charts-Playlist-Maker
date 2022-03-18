1b92a7c4b7cfcb8fb83e28c9582b7222836dd24e
Brooke Knowles <brookeknowles7@gmail.com>
2022-03-18 20:28:04 +1300

    fixed issues with scraping. now correctly returns top 100 songs and artists in json format

--- main.py
+++ main.py
@@ -5,35 +5,10 @@ import json
 url = "https://www.billboard.com/charts/hot-100/"
 result = requests.get(url)
 soup = BeautifulSoup(result.text, "html.parser")
+soup.find("div", class_="lxml")
 
-# print(soup.prettify())
-song_list = []
-artist_list = []
+song_list = [result.text.strip() for result in soup.select("div.chart-results-list > div.o-chart-results-list-row-container > ul.o-chart-results-list-row > li:nth-child(4) > ul > li:nth-child(1) h3")]
+artist_list = [result.text.strip() for result in soup.select("div.chart-results-list > div.o-chart-results-list-row-container> ul.o-chart-results-list-row > li:nth-child(4) > ul > li:nth-child(1) span")]
 
-# GET #1 ARTIST (CURRENTLY NOT WORKING)
-topArtist = soup.find_all("p", {"class": "c-tagline  a-font-primary-l a-font-primary-m@mobile-max lrv-u-color-black u-color-white@mobile-max lrv-u-margin-tb-00 lrv-u-padding-t-025 lrv-u-margin-r-150"})
-# print(topArtist)
-
-# GET #1 SONG (WORKS FINE)
-topSong = soup.find("a", {"href": "#",
-                          "class": "c-title__link lrv-a-unstyle-link"})
-song_list.append(topSong.text)
-
-
-# GET ARTISTS 2-100 (WORKS FINE)
-artist = soup.findAll("span", {"class": "c-label a-no-trucate a-font-primary-s lrv-u-font-size-14@mobile-max u-line"
-                                        "-height-normal@mobile-max u-letter-spacing-0021 lrv-u-display-block a-"
-                                        "truncate-ellipsis-2line u-max-width-330 u-max-width-230@tablet-only"
-                               })
-for i in range(99):
-    artist_list.append(artist[i].text)
-
-
-# GET SONGS 2-100 (WORKS FINE)
-song = soup.findAll("h3", {"class": "c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size"
-                                    "-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max "
-                                    "a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only",
-                           "id": "title-of-a-story"})
-
-for i in range(99):
-    song_list.append(song[i].text)
+chart_data = json.dumps([{'Artist': artists, 'Track': songs} for artists, songs in zip(artist_list, song_list)])
+print(chart_data)
