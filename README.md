995362e4af3babd721927686e91014ab4adb9b2f
Brooke Knowles <brookeknowles7@gmail.com>
2022-06-16 20:01:53 +1200

    add function to get NZtop40 data

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # music-charts
 
-Work in progress web scaper app used to get the data from the Billboard Hot 100 in a json format. Later to be used for creating playlists
+Work in progress web scaper app used to get the data from chart sites (like the Billboard Hot 100 and NZ Top 40) in a json format. Later to be used for creating playlists
 
 ### To run:
 Install dependencies:
@@ -8,6 +8,12 @@ Install dependencies:
 - pip install requests
 - flask
 - spotipy
+
+Run App (from terminal):
+
+> set FLASK_APP=app
+> 
+> flask run
 
 ### To do:
 - Make a script that downloads all the dependencies easy
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import re
 
 
 def get_billboard_hot_100():
@@ -27,6 +28,30 @@ def get_billboard_hot_100():
 
     return chart_data
 
+def get_NZ_top_40():
+    """ Gets the data from the NZTop40 website, and then creates a JSON object with all the relevant
+        information """
+
+    url = "https://nztop40.co.nz/chart/singles"
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, "html.parser")
+
+    song_list_raw = soup.findAll("h2", {"class": "title"})
+    song_list = []
+    for element in song_list_raw:
+        song_list.append(strip_punctuation(element.string))
+
+    artist_list_raw = soup.findAll("h3", {"class": "artist"})
+    artist_list = []
+    for element in artist_list_raw:
+        artist_list.append(strip_punctuation(element.string))
+
+    position_list = [i for i in range(1, 100 + 1)]
+
+    chart_data = json.dumps([{'Position': positions, 'Artist': artists, 'Track': songs} for positions, artists, songs in
+                             zip(position_list, artist_list, song_list)])
+
+    return chart_data
 
 def strip_punctuation(input_str):
     """ Spotify doesn't like special characters so best to remove punctuation """
@@ -40,3 +65,4 @@ def strip_punctuation(input_str):
     return input_str
 
 
+print(get_NZ_top_40())
